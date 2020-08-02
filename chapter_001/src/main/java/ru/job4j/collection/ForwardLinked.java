4cf03d87382ce93bde06b4957a037a8e9a61bf3b
c0dered273 <okaschuk@gmail.com>
2020-08-02 20:49:17 +0400

    Используя контейнер на базе связанного списка создать контейнер Stack [#288545]

--- chapter_001/src/main/java/ru/job4j/collection/ForwardLinked.java
+++ chapter_001/src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -1,37 +1,73 @@
 package ru.job4j.collection;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class ForwardLinked<T> implements Iterable<T> {
     private Node<T> head;
+    private int size;
+    private int modCount;
 
     public void add(T value) {
         Node<T> node = new Node<>(value, null);
         if (head == null) {
             head = node;
+            size++;
             return;
         }
-        Node<T> tail = head;
-        while (tail.next != null) {
-            tail = tail.next;
-        }
-        tail.next = node;
+        Node<T> lastNode = get(size - 1);
+        lastNode.next = node;
+        size++;
+        modCount++;
     }
 
     public T deleteFirst() {
+        checkHeadIsNull();
+        T oldValue = head.value;
+        head = head.next;
+        size--;
+        modCount++;
+        return oldValue;
+    }
+
+    public T deleteLast() {
+        checkHeadIsNull();
+        Node<T> beforeLast;
+        T lastValue;
+        if (size == 1) {
+            lastValue = head.value;
+            head = null;
+        } else {
+            beforeLast = get(size - 2);
+            lastValue = beforeLast.next.value;
+            beforeLast.next = null;
+        }
+        size--;
+        modCount--;
+        return lastValue;
+    }
+
+    public Node<T> get(int index) {
+        checkHeadIsNull();
+        Node<T> result = head;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result;
+    }
+
+    private void checkHeadIsNull() {
         if (head == null) {
             throw new NoSuchElementException();
         }
-        T oldValue = head.value;
-        head = head.next;
-        return oldValue;
     }
 
     @Override
     public Iterator<T> iterator() {
         return new Iterator<>() {
             Node<T> node = head;
+            private final int expectedModCount = modCount;
 
             @Override
             public boolean hasNext() {
@@ -40,6 +76,9 @@ public class ForwardLinked<T> implements Iterable<T> {
 
             @Override
             public T next() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
                 if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
--- chapter_001/src/test/java/ru/job4j/collection/ForwardLinkedTest.java
+++ chapter_001/src/test/java/ru/job4j/collection/ForwardLinkedTest.java
@@ -19,9 +19,23 @@ public class ForwardLinkedTest {
     }
 
     @Test(expected = NoSuchElementException.class)
+    public void whenDeleteLast() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.deleteLast();
+        linked.iterator().next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
     public void whenDeleteEmptyLinked() {
         ForwardLinked<Integer> linked = new ForwardLinked<>();
         linked.deleteFirst();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteLastEmptyLinked() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.deleteLast();
     }
 
     @Test
@@ -32,5 +46,15 @@ public class ForwardLinkedTest {
         linked.deleteFirst();
         Iterator<Integer> it = linked.iterator();
         assertThat(it.next(), is(2));
+    }
+
+    @Test
+    public void whenMultiDeleteLast() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.add(2);
+        linked.deleteLast();
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(1));
     }
 }
