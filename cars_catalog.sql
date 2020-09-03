fad8e36b32e2d0a740b10828e5acae312115bd6b
c0dered273 <okaschuk@gmail.com>
2020-09-03 11:34:36 +0400

    car catalog

--- cars_catalog.sql
+++ cars_catalog.sql
@@ -41,13 +41,26 @@ insert into gearbox(name) values
 ('3stage');
 
 insert into car (name, body_id, engine_id, gearbox_id) values
-('kopeika', (select id from body as b where b.name = '2101'), (select id from engine as e where e.name = '1,2'), (select id from gearbox as g where g.name = '4stage')),
-('shesterka', (select id from body as b where b.name = '2106'), (select id from engine as e where e.name = '1,5'), (select id from gearbox as g where g.name = '5stage')),
-('devyatka', (select id from body as b where b.name = '2109'), (select id from engine as e where e.name = '1,6'), (select id from gearbox as g where g.name = '5stage')),
-('daemon_taz', (select id from body as b where b.name = '210666'), (select id from engine as e where e.name = '6,8'), (select id from gearbox as g where g.name = '3stage'));
+('kopeika', (select id from body as b where b.name = '2101'),
+            (select id from engine as e where e.name = '1,2'),
+            (select id from gearbox as g where g.name = '4stage')),
+('shesterka', (select id from body as b where b.name = '2106'),
+              (select id from engine as e where e.name = '1,5'),
+              (select id from gearbox as g where g.name = '5stage')),
+('devyatka', (select id from body as b where b.name = '2109'),
+             (select id from engine as e where e.name = '1,6'),
+             (select id from gearbox as g where g.name = '5stage')),
+('daemon_taz', (select id from body as b where b.name = '210666'),
+               (select id from engine as e where e.name = '6,8'),
+               (select id from gearbox as g where g.name = '3stage'));
 
 select c.name, b.name, e.name, g.name from car as c
 left join body as b on c.body_id = b.id
 left join engine as e on c.engine_id = e.id
 left join gearbox as g on c.gearbox_id = g.id
 
+select b.name from body as b left outer join car as c on b.id = c.body_id where c.body_id is null;
+
+select e.name from engine as e left outer join car as c on e.id = c.engine_id where c.engine_id is null;
+
+select g.name from gearbox as g left outer join car as c on g.id = c.gearbox_id where c.gearbox_id is null;
