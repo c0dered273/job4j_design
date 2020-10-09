0eb2ef095f8e03dadc42b78af49b81c2d930fc5d
c0dered273 <okaschuk@gmail.com>
2020-10-09 14:13:02 +0400

    Шаблонизатор. [#288612]

---
+++ chapter_005/src/test/java/ru/job4j/tdd/GeneratorTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.tdd;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class GeneratorTest {
+
+    @Test
+    public void whenProduce() {
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> keys = Map.of(
+                "name", "Vasya",
+                "subject", "you");
+        Generator generator = new GeneratorImpl();
+        String result = generator.produce(template, keys);
+        String expect = "I am a Vasya, Who are you?";
+        assertThat(result, is(expect));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenProduceWithException() {
+        String template = "I am a ${name}, Who are ${subject}. What is you ${age}?";
+        Map<String, String> keys = Map.of(
+                "name", "Vasya",
+                "subject", "you");
+        Generator generator = new GeneratorImpl();
+        String result = generator.produce(template, keys);
+        String expect = "I am a Vasya, Who are you?";
+        assertThat(result, is(expect));
+    }
+}
