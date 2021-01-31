322a16f3067b36742be2ddd7d06dda76a8a7482a
c0dered273 <okaschuk@gmail.com>
2021-01-31 20:20:13 +0300

    Прерывание нити [#458259]

---
+++ chapter_007/src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,27 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        int count = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            char[] process = {'-', '\\', '|', '/'};
+            System.out.print("\r load: " + process[count++]);
+            if (count == 4) {
+                count = 0;
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new ConsoleProgress());
+        thread.start();
+        Thread.sleep(5000);
+        thread.interrupt();
+    }
+}
