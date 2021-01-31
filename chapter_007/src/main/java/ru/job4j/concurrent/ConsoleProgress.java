0abbae81af8512a915e3d2a2340f66c65aefd721
c0dered273 <okaschuk@gmail.com>
2021-01-31 20:45:34 +0300

    Прерывание нити [#458259]

--- chapter_007/src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ chapter_007/src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -13,7 +13,7 @@ public class ConsoleProgress implements Runnable {
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
-                return;
+                Thread.currentThread().interrupt();
             }
         }
     }
