43f2ab72583ee5bb7b40c1a295a23b3409e66725
c0dered273 <okaschuk@gmail.com>
2021-02-17 19:44:14 +0300

    Visibility. Общий ресурс вне критической секции [#458269]

--- chapter_007/src/main/java/ru/job4j/synchro/ParseFile.java
+++ chapter_007/src/main/java/ru/job4j/synchro/ParseFile.java
@@ -13,32 +13,37 @@ public class ParseFile {
         return file;
     }
 
-    public String getContent() throws IOException {
-        InputStream i = new FileInputStream(file);
-        String output = "";
-        int data;
-        while ((data = i.read()) > 0) {
-            output += (char) data;
-        }
-        return output;
-    }
-
-    public String getContentWithoutUnicode() throws IOException {
-        InputStream i = new FileInputStream(file);
-        String output = "";
-        int data;
-        while ((data = i.read()) > 0) {
-            if (data < 0x80) {
-                output += (char) data;
+    public synchronized String getContent() throws IOException {
+        StringBuilder output;
+        try (InputStream i = new FileInputStream(file)) {
+            output = new StringBuilder();
+            int data;
+            while ((data = i.read()) > 0) {
+                output.append((char) data);
             }
         }
-        return output;
+        return output.toString();
     }
 
-    public void saveContent(String content) throws IOException {
-        OutputStream o = new FileOutputStream(file);
-        for (int i = 0; i < content.length(); i += 1) {
-            o.write(content.charAt(i));
+    public synchronized String getContentWithoutUnicode() throws IOException {
+        StringBuilder output;
+        try (InputStream i = new FileInputStream(file)) {
+            output = new StringBuilder();
+            int data;
+            while ((data = i.read()) > 0) {
+                if (data < 0x80) {
+                    output.append((char) data);
+                }
+            }
+        }
+        return output.toString();
+    }
+
+    public synchronized void saveContent(String content) throws IOException {
+        try (OutputStream o = new FileOutputStream(file)) {
+            for (int i = 0; i < content.length(); i += 1) {
+                o.write(content.charAt(i));
+            }
         }
     }
 }
