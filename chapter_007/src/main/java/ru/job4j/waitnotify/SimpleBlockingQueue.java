64b1bb82f8009b84610d8d5220ef18f6aa78a774
c0dered273 <okaschuk@gmail.com>
2021-03-04 20:30:57 +0300

    Реализовать шаблон Producer Consumer. [#458251] результат checkstyle

--- chapter_007/src/main/java/ru/job4j/waitnotify/SimpleBlockingQueue.java
+++ chapter_007/src/main/java/ru/job4j/waitnotify/SimpleBlockingQueue.java
@@ -1,28 +1,37 @@
 package ru.job4j.waitnotify;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
+/**
+ * JavaDoc.
+ *
+ * @param <T> param
+ */
 @ThreadSafe
 public class SimpleBlockingQueue<T> {
 
-    @GuardedBy("this")
-    private final Queue<T> queue = new LinkedList<>();
+  @GuardedBy("this")
+  private final Queue<T> queue = new LinkedList<>();
 
-    public synchronized void offer(T value) {
-        queue.offer(value);
-        this.notifyAll();
+  public synchronized void offer(T value) {
+    queue.offer(value);
+    this.notifyAll();
+  }
+
+  /**
+   * JavaDoc.
+   *
+   * @return return
+   * @throws InterruptedException interrupt
+   */
+  public synchronized T poll() throws InterruptedException {
+    T rsl;
+    while ((rsl = queue.poll()) == null) {
+      this.wait();
     }
-
-    public synchronized T poll() throws InterruptedException {
-        T rsl;
-        while ((rsl = queue.poll()) == null) {
-            this.wait();
-
-        }
-        return rsl;
-    }
+    return rsl;
+  }
 }
