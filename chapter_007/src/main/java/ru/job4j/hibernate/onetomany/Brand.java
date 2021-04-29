4a77d8f01675e6f1f21fb6ccaf87d92a9ca9ccce
c0dered273 <okaschuk@gmail.com>
2021-04-29 11:30:20 +0300

    LazyInitializationexception [#458175]

--- chapter_007/src/main/java/ru/job4j/hibernate/onetomany/Brand.java
+++ chapter_007/src/main/java/ru/job4j/hibernate/onetomany/Brand.java
@@ -1,77 +1,77 @@
-package ru.job4j.hibernate.onetomany;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
-@Entity
-@Table(name = "brands")
-public class Brand {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    private String name;
-
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Model> models = new ArrayList<>();
-
-    protected Brand() {
-    }
-
-    public static Brand of(String name) {
-        var brand = new Brand();
-        brand.setName(name);
-        return brand;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Model> getModels() {
-        return models;
-    }
-
-    public void setModels(List<Model> models) {
-        this.models = models;
-    }
-
-    public void addModel(Model m) {
-        this.models.add(m);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Brand brand = (Brand) o;
-        return id == brand.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Brand{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", models=" + models +
-                '}';
-    }
-}
+//package ru.job4j.hibernate.onetomany;
+//
+//import javax.persistence.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Objects;
+//
+//@Entity
+//@Table(name = "brands")
+//public class Brand {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private int id;
+//    private String name;
+//
+//    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Model> models = new ArrayList<>();
+//
+//    protected Brand() {
+//    }
+//
+//    public static Brand of(String name) {
+//        var brand = new Brand();
+//        brand.setName(name);
+//        return brand;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public List<Model> getModels() {
+//        return models;
+//    }
+//
+//    public void setModels(List<Model> models) {
+//        this.models = models;
+//    }
+//
+//    public void addModel(Model m) {
+//        this.models.add(m);
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Brand brand = (Brand) o;
+//        return id == brand.id;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(id);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Brand{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", models=" + models +
+//                '}';
+//    }
+//}
--- chapter_007/src/main/java/ru/job4j/hibernate/onetomany/Model.java
+++ chapter_007/src/main/java/ru/job4j/hibernate/onetomany/Model.java
@@ -1,59 +1,59 @@
-package ru.job4j.hibernate.onetomany;
-
-import javax.persistence.*;
-import java.util.Objects;
-
-@Entity
-@Table(name = "models")
-public class Model {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    private String name;
-
-    protected Model() {
-    }
-
-    public static Model of(String name) {
-        var model = new Model();
-        model.setName(name);
-        return model;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Model model = (Model) o;
-        return id == model.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Model{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
-}
+//package ru.job4j.hibernate.onetomany;
+//
+//import javax.persistence.*;
+//import java.util.Objects;
+//
+//@Entity
+//@Table(name = "models")
+//public class Model {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private int id;
+//    private String name;
+//
+//    protected Model() {
+//    }
+//
+//    public static Model of(String name) {
+//        var model = new Model();
+//        model.setName(name);
+//        return model;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Model model = (Model) o;
+//        return id == model.id;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(id);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Model{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                '}';
+//    }
+//}
